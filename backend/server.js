af4b81a443934c3b31daf04b366389dba651c68d
Ishant-179 <rajishant128@gmail.com>
2025-06-24 20:25:07 +0530

    Initial commit with frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin:[
-        'https://shopistry-fr.vercel.app/',
+        'https://shopistry-fr.vercel.app',
         'http://localhost:5173'
     ] ,          
     credentials: true
